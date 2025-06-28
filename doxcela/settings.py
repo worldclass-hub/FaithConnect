ab3244d2ebe8ecfea7172d35cae21cad4fec131a
Mac <mac@Macs-MacBook-Pro.local>
2025-06-28 21:13:54 +0100

    Doxcela

--- doxcela/settings.py
+++ doxcela/settings.py
@@ -205,7 +205,7 @@ import dj_database_url
 from decouple import config
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
