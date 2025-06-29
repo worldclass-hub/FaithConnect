38e29182e9ea508ffc35f9076bb35d2ddabdef80
Mac <mac@Macs-MacBook-Pro.local>
2025-06-29 23:51:23 +0100

    doxcela

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
 import os
-from doxcela.wsgi import application
-from gunicorn.app.wsgiapp import run
+from gunicorn.app.wsgiapp import WSGIApplication
 
-if __name__ == '__main__':
-    port = os.environ.get("PORT", "8000")
-    run(["gunicorn", "doxcela.wsgi:application", "--bind", f"0.0.0.0:{port}"])
+if __name__ == "__main__":
+    port = os.getenv("PORT", "8000")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "doxcela.settings")  # Replace if your settings file path is different
+    app = WSGIApplication()
+    app.run(bind=f"0.0.0.0:{port}", args=["gunicorn", "doxcela.wsgi:application"])
